b1f6d252f1f4cf4f96c8decbcb38f8fa93abcc70
Natalia Venditto <nataliafrontend@gmail.com>
2024-03-12 14:30:04 +0100

    ci: enable devserver for tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,6 +5,7 @@ import { defineConfig, devices } from '@playwright/test';
  * https://github.com/motdotla/dotenv
  */
 // require('dotenv').config();
+const useLocalServer = false;
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -68,10 +69,10 @@ export default defineConfig({
     // },
   ],
 
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  //* Run your local dev server before starting the tests */
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:5173',
+    reuseExistingServer: useLocalServer || !process.env.CI,
+  },
 });
