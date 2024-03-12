dbe1410221d6846a6286a955845240e4be55215a
Natalia Venditto <nataliafrontend@gmail.com>
2024-03-12 15:10:12 +0100

    ci: reconfig gha for playwright

--- tests/translator.spec.ts
+++ tests/translator.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect, type Page } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('http://localhost:5173/');
+  await page.goto('/');
 });
 
 const WORDS_TO_TRANSLATE = [
